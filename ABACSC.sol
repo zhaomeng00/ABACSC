1b1a91d1d0db2bc5dc1b5304cd276e4f1a9acfe0
zhaomeng00 <46369119+zhaomeng00@users.noreply.github.com>
2019-01-04 16:06:13 +0800

    Add files via upload

---
+++ ABACSC.sol
@@ -0,0 +1,70 @@
+pragma solidity ^0.4.16;
+
+contract ABACSC {
+    bool public status;
+    address public owner;
+	uint public numberOfUsers;
+    mapping (address => uint) public userId;
+    User[] public users;
+
+    event UserAdd(address UserAddress, string UserAttributes, string UserNotes);
+    event UserRemoved(address UserAddress);
+    event StatusChanged(string Status);
+
+    struct User {
+        address user;
+        string attributes;
+        string notes;
+        uint userSince;
+    }
+
+    modifier onlyUsers {
+        require(userId[msg.sender] != 0);
+        _;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor (string enterOrganizationName) public {
+        owner = msg.sender;
+        status = true;
+        addUser(0, "", "");
+        addUser(owner, 'Creator and Owner of Smart Contract', "");
+        numberOfUsers = 0;
+    }
+
+    function changeStatus (bool deactivate) onlyOwner public {
+        if (deactivate)
+        {status = false;}
+        emit StatusChanged("Smart Contract Deactivated");
+    }
+
+    function addUser(address userAddress, string userAttributes, string userNotes) onlyOwner public {
+        require(status = true);
+        uint id = userId[userAddress];
+        if (id == 0) {
+            userId[userAddress] = users.length;
+            id = users.length++;
+        }
+        users[id] = User({user: userAddress, userSince: now, attributes:userAttributes, notes: userNotes});
+        emit UserAdd(userAddress, userAttributes, userNotes);
+        numberOfUsers++;
+    }
+
+    function removeUser(address userAddress) onlyOwner public {
+        require(userId[userAddress] != 0);
+        for (uint i = userId[userAddress]; i<users.length-1; i++){
+            users[i] = users[i+1];
+        }
+        delete users[users.length-1];
+		userId[userAddress] = 0;
+        users.length--;
+        emit UserRemoved(userAddress);
+        numberOfUsers--;
+    }
+
+ 
+}
